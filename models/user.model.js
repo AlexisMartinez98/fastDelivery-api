53f98c72bede6abc14a719737acdd4561bec24f0
Micaela <micaelauvilla9@gmail.com>
2023-09-19 16:40:11 -0300

    changed user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,21 +1,9 @@
 const mongoose = require("mongoose");
 const modelOptions = require("./model.options");
-
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
-    first_name: {
-      type: String,
-      required: true,
-    },
-    last_name: {
-      type: String,
-      required: true,
-    },
-    url_img: {
-      type: String,
-      required: false,
-    },
     email: {
       type: String,
       required: true,
@@ -26,17 +14,33 @@ const userSchema = new mongoose.Schema(
       required: true,
       select: false,
     },
+    confirm_password: {
+      type: String,
+      require: true,
+      select: false,
+    },
     salt: {
       type: String,
       required: true,
       select: false,
     },
-
+    is_admin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { modelOptions }
 );
+userSchema.methods.setPassword = async function (password) {
+  const salt = bcrypt.genSaltSync(8);
+  this.salt = salt;
+  this.password = await bcrypt.hash(password, this.salt);
+};
 
-
+userSchema.methods.validPassword = async function (password) {
+  const hashPass = await bcrypt.hash(password, this.salt);
+  return this.password === hashPass;
+};
 
 const userModel = mongoose.model("User", userSchema);
 
